3986048a1af0462124f9caf005248358c14bed33
TETEURYAN <matheusryan61@gmail.com>
2023-03-09 20:14:55 -0300

    commit

---
+++ src/calcs.h
@@ -0,0 +1,14 @@
+#ifdef CALCS.H
+#define CALCS.H
+
+double (double)
+
+double area_coroa(double, double );
+
+double area_circunferencia(double );
+
+double comprimemnto_circunferencia(double );
+
+double area_setor(double , double );
+
+#endif
