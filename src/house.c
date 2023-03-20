c9d71502ca36f829b0fe85269cea91477d9f77b0
Matheus Ryan <91018438+TETEURYAN@users.noreply.github.com>
2023-03-19 23:01:27 -0300

    Add Rooms

--- src/house.c
+++ src/house.c
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
-#include <GL/glut.h>
-#include <math.h>
-#include "house.h"
-
-
-void MakeElipse(GLfloat x, GLfloat y, GLfloat radius)
-{
-    glBegin(GL_LINE_LOOP);
-        for(int i=0; i<360; i++) {
-        GLfloat angle = i * 3.14159265358979323846 / 180.0; // Converte graus para radianos
-        glVertex2f(x + radius * cos(angle), y + radius * sin(angle));
-        }
-    glEnd();
-}
-
-void MakeFirstRoom(GLfloat x, GLfloat y, GLfloat radius)
-{
-    //MakeElipse(0.0, 0, 150.0);// primeiro andar ou terreo
-
-    glBegin(GL_LINE_LOOP);
-        for(int i=0; i<360; i++) {
-        GLfloat angle = i * 3.14159265358979323846 / 180.0; // Converte graus para radianos
-        glVertex2f(x + radius * cos(angle), y + radius * sin(angle));
-        }
-    glEnd();
-
-    MakeElipse(0.0, 0, radius - 5);// paredes primeiro andar ou terreo
-}
-
-=======
 #include "calcs.h"
 #include "drawning.h"
 #include "house.h"
@@ -108,4 +77,3 @@ void drawRoom(Room * comodo)
     glEnd();
 
 }
->>>>>>> Stashed changes
